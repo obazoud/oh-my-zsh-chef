27baec6da82177b6baff6bf7ec649601c700f8fb
Olivier Bazoud <olivier.bazoud@gmail.com>
2015-03-03 14:17:40 +0100

    Fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,5 @@ default['oh_my_zsh'][:users] = []
 default['oh_my_zsh'][:reference] = "master"
 default['oh_my_zsh'][:revision] = "master"
 default['oh_my_zsh'][:repository] = "git://github.com/robbyrussell/oh-my-zsh.git"
+default['oh_my_zsh'][:checkout_branch] = "deploy"
 default['oh_my_zsh'][:install_git] = true
